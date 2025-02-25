9bc0a677854daa1ffba7675114707c1e33745873
gadsarif <gads.arif@gmail.com>
2025-02-25 22:13:58 +0600

    2nd update name krishno

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="mainsection">
         <header className="">
           <h1 className="text-white text-center text-7xl">
-            This is 1st Krishno
+            This is 2st Krishno
           </h1>
         </header>
       </div>
