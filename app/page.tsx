144dd17459978f0179e705a00f3a088dda0dbdf4
gadsarif <gads.arif@gmail.com>
2025-02-25 22:05:04 +0600

    Update all changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="mainsection">
         <header className="">
           <h1 className="text-white text-center text-7xl">
-            This is 2nd git update
+            This is 1st git update
           </h1>
         </header>
       </div>
